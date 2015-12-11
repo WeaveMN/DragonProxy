77109cb177bd24092b8ad1365ce89fa1e465dd48
DefinitlyEvil <theevildays@gmail.com>
2015-12-11 22:40:24 +0800

    multi-language framework

--- src/main/java/org/dragonet/proxy/DragonProxy.java
+++ src/main/java/org/dragonet/proxy/DragonProxy.java
@@ -12,8 +12,43 @@
  */
 package org.dragonet.proxy;
 
+import java.io.IOException;
+import lombok.Getter;
+import org.dragonet.proxy.config.Lang;
+import org.dragonet.proxy.config.ServerConfig;
+import org.dragonet.proxy.utilities.Versioning;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DragonProxy {
     public static void main(String[] args){
+        new DragonProxy().run(args);
+    }
+    
+    private final Logger logger = LoggerFactory.getLogger("DragonProxy");
+    
+    @Getter
+    private ServerConfig config;
+    
+    @Getter
+    private Lang lang;
+    
+    public void run(String[] args){
+        try {
+            config = new ServerConfig();
+        } catch (IOException ex) {
+            logger.error("Faild to load configuration file! ");
+            ex.printStackTrace();
+            return;
+        }
+        try {
+            lang = new Lang(config.getConfig().getProperty(ServerConfig.LANG_FILE));
+        } catch (IOException ex) {
+            logger.error("Faild to load language file! ");
+            ex.printStackTrace();
+            return;
+        }
+        logger.info(lang.get(Lang.INIT_LOADING, Versioning.RELEASE_VERSION));
         
     }
 }
---
+++ src/main/java/org/dragonet/proxy/utilities/Versioning.java
@@ -0,0 +1,17 @@
+/*
+ * GNU LESSER GENERAL PUBLIC LICENSE
+ *                       Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ *
+ * You can view LICENCE file for details. 
+ *
+ * @author The Dragonet Team
+ */
+package org.dragonet.proxy.utilities;
+
+public class Versioning {
+    public final static String RELEASE_VERSION = "0.0.1";
+}
