14e0548d7739c62a99a226d6d84021b175b1efc9
DefinitlyEvil <theevildays@gmail.com>
2015-12-14 16:22:36 +0800

    oops forgot registration of LoginSuccessMessage

--- src/main/java/net/glowstone/net/protocol/LoginProtocol.java
+++ src/main/java/net/glowstone/net/protocol/LoginProtocol.java
@@ -5,13 +5,16 @@ import net.glowstone.net.codec.SetCompressionCodec;
 import net.glowstone.net.codec.login.EncryptionKeyRequestCodec;
 import net.glowstone.net.codec.login.EncryptionKeyResponseCodec;
 import net.glowstone.net.codec.login.LoginStartCodec;
+import net.glowstone.net.codec.login.LoginSuccessCodec;
 import net.glowstone.net.message.KickMessage;
 import net.glowstone.net.message.SetCompressionMessage;
 import net.glowstone.net.message.login.EncryptionKeyRequestMessage;
 import net.glowstone.net.message.login.EncryptionKeyResponseMessage;
 import net.glowstone.net.message.login.LoginStartMessage;
+import net.glowstone.net.message.login.LoginSuccessMessage;
 import org.dragonet.proxy.network.handler.login.EncryptionKeyRequestHandler;
 import org.dragonet.proxy.network.handler.login.KickHandler;
+import org.dragonet.proxy.network.handler.login.LoginSuccessHandler;
 import org.dragonet.proxy.network.handler.login.SetCompressionHandler;
 
 public final class LoginProtocol extends GlowProtocol {
@@ -23,7 +26,7 @@ public final class LoginProtocol extends GlowProtocol {
 
         inbound(0x01, EncryptionKeyRequestMessage.class, EncryptionKeyRequestCodec.class, EncryptionKeyRequestHandler.class);
         inbound(0x00, KickMessage.class, KickCodec.class, KickHandler.class);
-        //inbound(0x02, LoginSuccessMessage.class, LoginSuccessCodec.class);
+        inbound(0x02, LoginSuccessMessage.class, LoginSuccessCodec.class, LoginSuccessHandler.class);
         inbound(0x03, SetCompressionMessage.class, SetCompressionCodec.class, SetCompressionHandler.class);
     }
 }
