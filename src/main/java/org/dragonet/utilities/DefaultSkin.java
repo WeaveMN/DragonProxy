88a2097e9a3e1dcd29126a0a37094af5f6e959ed
Julien Lirochon <julien@lirochon.net>
2015-12-15 15:28:59 +0100

    adds default skin

---
+++ src/main/java/org/dragonet/utilities/DefaultSkin.java
@@ -0,0 +1,33 @@
+package org.dragonet.utilities;
+
+import java.io.*;
+
+public class DefaultSkin {
+
+    protected static byte[] defaultSkin;
+
+    static {
+        loadSkin();
+    }
+
+    public static byte[] getDefaultSkin() {
+        return defaultSkin;
+    }
+
+    private static void loadSkin() {
+        try {
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(DefaultSkin.class.getResourceAsStream("/defaults/SKIN.bin")));
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            String hex = in.readLine();
+            defaultSkin = Binary.hexStringToBytes(hex);
+            out.close();
+            in.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
