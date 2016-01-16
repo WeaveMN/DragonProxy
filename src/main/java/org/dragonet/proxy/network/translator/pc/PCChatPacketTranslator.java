5f92a75490a62159f91f2573d3fd5a528b490d0a
Leonardo <leonardomalaman@gmail.com>
2016-01-16 07:50:53 -0200

    Transform JSON Messages into plain text.

--- src/main/java/org/dragonet/proxy/network/translator/pc/PCChatPacketTranslator.java
+++ src/main/java/org/dragonet/proxy/network/translator/pc/PCChatPacketTranslator.java
@@ -12,6 +12,11 @@
  */
 package org.dragonet.proxy.network.translator.pc;
 
+import java.util.Iterator;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.dragonet.net.packet.minecraft.ChatPacket;
 import org.dragonet.net.packet.minecraft.PEPacket;
 import org.dragonet.proxy.network.UpstreamSession;
@@ -19,15 +24,39 @@ import org.dragonet.proxy.network.translator.PCPacketTranslator;
 import org.spacehq.mc.protocol.packet.ingame.server.ServerChatPacket;
 
 public class PCChatPacketTranslator implements PCPacketTranslator<ServerChatPacket> {
-
+	String chatMessage = "";
+	
     @Override
     public PEPacket[] translate(UpstreamSession session, ServerChatPacket packet) {
         ChatPacket ret = new ChatPacket();
+        /*
+         * Reset the chat message so we can parse the JSON again (if needed)
+         */
+        chatMessage = "";
         ret.source = "";
         ret.message = packet.getMessage().getFullText();
         switch (packet.getType()) {
             case CHAT:
                 ret.type = ChatPacket.TextType.CHAT;
+                /*
+                 * It is a JSON message?
+                 */
+				try {
+					JSONObject jObject = new JSONObject(ret.message);
+					/*
+					 * Let's iterate!
+					 */
+					handleKeyObject(jObject);
+					ret.message = chatMessage;
+				} catch (JSONException e) {
+					/*
+					 * If any exceptions happens, then:
+					 * * The JSON message is buggy or
+					 * * It isn't a JSON message
+					 * 
+					 * So, if any exceptions happens, we send the original message
+					 */
+				} 
                 break;
             case NOTIFICATION:
             case SYSTEM:
@@ -37,5 +66,54 @@ public class PCChatPacketTranslator implements PCPacketTranslator<ServerChatPack
         }
         return new PEPacket[]{ret};
     }
-
+    
+    public void handleKeyObject(JSONObject jObject) throws JSONException {
+    	Iterator<String> iter = jObject.keys();
+		while (iter.hasNext()) {
+		    String key = iter.next();
+		    try {
+		    	if (key.equals("text")) {
+		    		/*
+		    		 * We only need the text message from the JSON.
+		    		 */
+		    		String jsonMessage = jObject.getString(key);
+		    		chatMessage = chatMessage + jsonMessage;
+		    		continue;
+		    	}
+		    	if (jObject.get(key) instanceof JSONArray) {
+		    		handleKeyArray(jObject.getJSONArray(key));
+		    	}
+		    	if (jObject.get(key) instanceof JSONObject) {
+		    		handleKeyObject(jObject.getJSONObject(key));
+		    	}
+		    } catch (JSONException e) {
+		    }
+		}
+    }
+    
+    
+    public void handleKeyArray(JSONArray jObject) throws JSONException {
+    	JSONObject jsonObject = jObject.toJSONObject(jObject);
+    	Iterator<String> iter = jsonObject.keys();
+		while (iter.hasNext()) {
+		    String key = iter.next();
+		    try {
+	    		/*
+	    		 * We only need the text message from the JSON.
+	    		 */
+		    	if (key.equals("text")) {
+		    		String jsonMessage = jsonObject.getString(key);
+		    		chatMessage = chatMessage + jsonMessage;
+		    		continue;
+		    	}
+		    	if (jsonObject.get(key) instanceof JSONArray) {
+		    		handleKeyArray(jsonObject.getJSONArray(key));
+		    	}
+		    	if (jsonObject.get(key) instanceof JSONObject) {
+		    		handleKeyObject(jsonObject.getJSONObject(key));
+		    	}
+		    } catch (JSONException e) {
+		    }
+		}
+    }
 }
