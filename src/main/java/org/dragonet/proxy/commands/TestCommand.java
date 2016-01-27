750956fb7c731a0b13f15cf2c0a94030ef6254c5
DefinitlyEvil <theevildays@gmail.com>
2016-01-28 01:13:22 +0800

    added ChangeDimensionPacket lol

--- src/main/java/org/dragonet/proxy/commands/TestCommand.java
+++ src/main/java/org/dragonet/proxy/commands/TestCommand.java
@@ -12,9 +12,9 @@
  */
 package org.dragonet.proxy.commands;
 
-import org.dragonet.net.packet.minecraft.BlockEntityDataPacket;
+import org.dragonet.net.packet.minecraft.ChangeDimensionPacket;
+import org.dragonet.net.packet.minecraft.LoginStatusPacket;
 import org.dragonet.proxy.DragonProxy;
-import org.dragonet.proxy.nbt.tag.CompoundTag;
 import org.dragonet.proxy.network.UpstreamSession;
 
 public class TestCommand implements ConsoleCommand {
@@ -22,24 +22,10 @@ public class TestCommand implements ConsoleCommand {
     @Override
     public void execute(DragonProxy proxy, String[] args) {
         UpstreamSession cli = proxy.getSessionRegister().getAll().values().toArray(new UpstreamSession[0])[0];
-        cli.sendChat("Opening window... ");
-
-        int x = Integer.parseInt(args[0]);
-        int y = Integer.parseInt(args[1]);
-        int z = Integer.parseInt(args[2]);
-
-        CompoundTag tag = new CompoundTag();
-        tag.putString("id", "Sign");
-        tag.putInt("x", x);
-        tag.putInt("y", y);
-        tag.putInt("z", z);
-        tag.putString("Text1", "TEST 1");
-        tag.putString("Text2", "TEST 2");
-        tag.putString("Text3", "TEST 3");
-        tag.putString("Text4", "TEST 4");
-
-        BlockEntityDataPacket bed = new BlockEntityDataPacket(x, y, z, tag);
-        cli.sendPacket(bed);
+        cli.sendPacket(new ChangeDimensionPacket(0f, 64f, 0f, (byte)0));
+        LoginStatusPacket stat = new LoginStatusPacket();
+        stat.status = LoginStatusPacket.PLAYER_SPAWN;
+        cli.sendPacket(stat);
     }
 
 }
